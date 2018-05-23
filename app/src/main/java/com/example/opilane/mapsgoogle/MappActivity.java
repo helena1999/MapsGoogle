7b2b43e36bbc05c0ff2605ceac96eb92a0765ba2
opilane <helena.tarum@gmail.com>
2018-05-23 11:38:14 +0300

    1

--- app/src/main/java/com/example/opilane/mapsgoogle/MappActivity.java
+++ app/src/main/java/com/example/opilane/mapsgoogle/MappActivity.java
@@ -1,29 +1,57 @@
 package com.example.opilane.mapsgoogle;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.Manifest;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.Api;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
-public class MappActivity extends AppCompatActivity implements OnMapReadyCallback {
+import java.io.FileDescriptor;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class MappActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TAG = "MappActivity";
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
@@ -33,6 +61,13 @@ public class MappActivity extends AppCompatActivity implements OnMapReadyCallbac
     private GoogleMap gKaart;
     private FusedLocationProviderClient fusedLocationProviderClient;
     private static final float DEFAULT_ZOOM = 15f;
+    private ImageView gpsnupp, asukohaInfo, asukohKaart;
+
+    private AutoCompleteTextView otsinguTekst;
+    private PlaceAutocompleteAdapter placeAutocompleteAdapter;
+    private GoogleApiClient mGoogleApiClient;
+    private static final LatLngBounds LAT_LNG_BOUNDS = new LatLngBounds(new LatLng(-40, -168), new LatLng(71, 136));
+    private PlaceInfo mPlace;
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -54,7 +89,149 @@ public class MappActivity extends AppCompatActivity implements OnMapReadyCallbac
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapp);
+        otsinguTekst = findViewById(R.id.searchText);
+        asukohaInfo = findViewById(R.id.ic_info);
+        asukohKaart = findViewById(R.id.ic_map);
+        gpsnupp = findViewById(R.id.ic_gps);
         getLocationPermission();
+    }
+    private void kaivita (){
+        Log.d(TAG, "Lähetestamine");
+
+        mGoogleApiClient = new GoogleApiClient().Builder(this).addApi(Places.GEO_DATA_API).addApi(Places.PLACE_DETECTION_API).enableAutoManage(this, (GoogleApiClient.OnConnectionFailedListener) this).build(); {
+            @Override
+            public boolean hasConnectedApi(@NonNull Api<?> api) {
+                return false;
+            }
+
+            @NonNull
+            @Override
+            public ConnectionResult getConnectionResult(@NonNull Api<?> api) {
+                return null;
+            }
+
+            @Override
+            public void connect() {
+
+            }
+
+            @Override
+            public ConnectionResult blockingConnect() {
+                return null;
+            }
+
+            @Override
+            public ConnectionResult blockingConnect(long l, @NonNull TimeUnit timeUnit) {
+                return null;
+            }
+
+            @Override
+            public void disconnect() {
+
+            }
+
+            @Override
+            public void reconnect() {
+
+            }
+
+            @Override
+            public PendingResult<Status> clearDefaultAccountAndReconnect() {
+                return null;
+            }
+
+            @Override
+            public void stopAutoManage(@NonNull FragmentActivity fragmentActivity) {
+
+            }
+
+            @Override
+            public boolean isConnected() {
+                return false;
+            }
+
+            @Override
+            public boolean isConnecting() {
+                return false;
+            }
+
+            @Override
+            public void registerConnectionCallbacks(@NonNull ConnectionCallbacks connectionCallbacks) {
+
+            }
+
+            @Override
+            public boolean isConnectionCallbacksRegistered(@NonNull ConnectionCallbacks connectionCallbacks) {
+                return false;
+            }
+
+            @Override
+            public void unregisterConnectionCallbacks(@NonNull ConnectionCallbacks connectionCallbacks) {
+
+            }
+
+            @Override
+            public void registerConnectionFailedListener(@NonNull OnConnectionFailedListener onConnectionFailedListener) {
+
+            }
+
+            @Override
+            public boolean isConnectionFailedListenerRegistered(@NonNull OnConnectionFailedListener onConnectionFailedListener) {
+                return false;
+            }
+
+            @Override
+            public void unregisterConnectionFailedListener(@NonNull OnConnectionFailedListener onConnectionFailedListener) {
+
+            }
+
+            @Override
+            public void dump(String s, FileDescriptor fileDescriptor, PrintWriter printWriter, String[] strings) {
+
+            }
+        }
+
+        placeAutocompleteAdapter = new PlaceAutocompleteAdapter(this, mGoogleApiClient, LAT_LNG_BOUNDS, null);
+
+        otsinguTekst.setAdapter(placeAutocompleteAdapter);
+
+        otsinguTekst.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+
+                if (actionId == EditorInfo.IME_ACTION_SEARCH || keyEvent.getAction() == keyEvent.ACTION_DOWN || keyEvent.getAction() == keyEvent.KEYCODE_ENTER){
+
+                    geoLocate();
+                }
+                return false;
+            }
+        });
+        gpsnupp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "GPS nupul vajutataud");
+                getSeadmeAsukoht();
+            }
+        });
+    }
+
+    private void geoLocate() {
+        Log.d(TAG, "GeoLocating");
+        String otsing = otsinguTekst.getText().toString();
+        Geocoder geocoder = new Geocoder(MappActivity.this);
+        List<Address> nimekiri = new ArrayList<>();
+
+        try {
+            nimekiri = geocoder.getFromLocationName(otsing, 1);
+        }
+        catch (IOException e){
+            Log.e(TAG, "Geolocate viga:" + e.getMessage());
+        }
+        if (nimekiri.size() > 0){
+            Address address = nimekiri.get (0);
+            Log.d(TAG, "Geolocate leidis üles asukoha: " + address.toString());
+        }
+        liigutaKaamerat(new LatLng(address.getLatitude(), address.getLongitude(), DEFAULT_ZOOM, address.getAddressLine(0)));
     }
 
     private void getLocationPermission() {
@@ -107,7 +284,7 @@ public class MappActivity extends AppCompatActivity implements OnMapReadyCallbac
                         if (task.isSuccessful()){
                             Toast.makeText(MappActivity.this, "Asukoht on tuvastatud", Toast.LENGTH_SHORT).show();
                             Location currentlocation = (Location)task.getResult();
-                            liigutaKaamerat (new LatLng(currentlocation.getLatitude(), currentlocation.getLatitude()));
+                            liigutaKaamerat (new LatLng(currentlocation.getLatitude(), currentlocation.getLatitude()), DEFAULT_ZOOM, "My location");
                         }
                         else {
                             Toast.makeText(MappActivity.this, "Asukoht ei olnud võimalik" + "tuvastada", Toast.LENGTH_SHORT).show();
@@ -123,10 +300,22 @@ public class MappActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     }
 
-    private void liigutaKaamerat(LatLng latLng, float zoom) {
+    private void liigutaKaamerat(LatLng latLng, float zoom, String pealkiri) {
         Log.d(TAG, "lat: " + latLng.latitude + "lng: " + latLng.longitude);
         gKaart.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+        if (!pealkiri.equals("My location")){
+            MarkerOptions markerOptions = new MarkerOptions().position(latLng).title(pealkiri);
+            gKaart.addMarker(markerOptions);
+        }
     }
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    
 }
 
 
